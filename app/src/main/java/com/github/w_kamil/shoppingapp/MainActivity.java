92eaf6a8ae1a546ab54003a628601d2a5e91ff3d
w-kamil <w-kamil@wp.pl>
2017-04-07 19:18:39 +0200

    Main activity layout

--- app/src/main/java/com/github/w_kamil/shoppingapp/MainActivity.java
+++ app/src/main/java/com/github/w_kamil/shoppingapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.github.w_kamil.shoppingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.goto_scanner_textview) void startScanner(){
+        Toast.makeText(this, "skaner", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.goto_shops_textview) void startShopsActivity(){
+        Toast.makeText(this, "lista sklepów", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.goto_products_textview) void gotoProductsActivity(){
+        Toast.makeText(this, "lista produktów", Toast.LENGTH_SHORT).show();
+    }
+
 }
