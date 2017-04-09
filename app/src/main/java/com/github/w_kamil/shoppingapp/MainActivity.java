46e51c10801226b6bda9c670cf0685fe0d021d8b
w-kamil <w-kamil@wp.pl>
2017-04-09 18:00:51 +0200

    scanner function add

--- app/src/main/java/com/github/w_kamil/shoppingapp/MainActivity.java
+++ app/src/main/java/com/github/w_kamil/shoppingapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.github.w_kamil.shoppingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,17 +18,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
     }
 
-    @OnClick(R.id.goto_scanner_textview) void startScanner(){
-        Toast.makeText(this, "skaner", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(this, R.string.scan_cancelled, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "scanned: " + intentResult.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
-    @OnClick(R.id.goto_shops_textview) void startShopsActivity(){
+    @OnClick(R.id.goto_scanner_textview)
+    void startScanner() {
+        Toast.makeText(this, getString(R.string.scan_product), Toast.LENGTH_SHORT).show();
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
+        integrator.setPrompt(getString(R.string.scan_product));
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+    }
+
+    @OnClick(R.id.goto_shops_textview)
+    void startShopsActivity() {
         Toast.makeText(this, "lista sklepów", Toast.LENGTH_SHORT).show();
     }
 
-    @OnClick(R.id.goto_products_textview) void gotoProductsActivity(){
+    @OnClick(R.id.goto_products_textview)
+    void gotoProductsActivity() {
         Toast.makeText(this, "lista produktów", Toast.LENGTH_SHORT).show();
     }
 
