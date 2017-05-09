4466ccf3fb7503fe01fb696e04aac6bee5b34af9
w-kamil <w-kamil@wp.pl>
2017-05-09 14:35:30 +0200

    shops activity layout

--- app/src/main/java/com/github/w_kamil/shoppingapp/shops/ShopsActivity.java
+++ app/src/main/java/com/github/w_kamil/shoppingapp/shops/ShopsActivity.java
@@ -2,14 +2,28 @@ package com.github.w_kamil.shoppingapp.shops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.github.w_kamil.shoppingapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ShopsActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
+        ButterKnife.bind(this);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                LinearLayoutManager.VERTICAL));
+        
     }
 }
