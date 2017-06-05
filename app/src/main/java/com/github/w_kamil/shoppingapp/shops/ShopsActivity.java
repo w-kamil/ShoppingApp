9ffbfdf2bde7833830ec449a9305b9850179e939
w-kamil <w-kamil@wp.pl>
2017-06-05 09:54:30 +0200

    addNewShop dialog

--- app/src/main/java/com/github/w_kamil/shoppingapp/shops/ShopsActivity.java
+++ app/src/main/java/com/github/w_kamil/shoppingapp/shops/ShopsActivity.java
@@ -1,5 +1,7 @@
 package com.github.w_kamil.shoppingapp.shops;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
@@ -7,11 +9,16 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.github.w_kamil.shoppingapp.R;
+import com.github.w_kamil.shoppingapp.dao.Product;
 import com.github.w_kamil.shoppingapp.dao.Shop;
 import com.github.w_kamil.shoppingapp.dao.ShoppingDatabaseDao;
 
@@ -25,6 +32,7 @@ public class ShopsActivity extends AppCompatActivity implements PopupMenu.OnMenu
 
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
+    private ShoppingDatabaseDao dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +44,10 @@ public class ShopsActivity extends AppCompatActivity implements PopupMenu.OnMenu
 
 
         //TODO implement presnter for providing data to ShopsActivity
-        ShoppingDatabaseDao dao = new ShoppingDatabaseDao(this);
-        List<Shop> shops = dao.fetchAllShops();
-
-        ShopsListAdapter adapter = new ShopsListAdapter(shops);
-        adapter.setOnMenuItemClickListener(this);
-        recyclerView.setAdapter(adapter);
+        dao = new ShoppingDatabaseDao(this);
+        updateUI();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,10 +58,35 @@ public class ShopsActivity extends AppCompatActivity implements PopupMenu.OnMenu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        //TODO implement adding new shop to list popup window and list settings/filter
         switch (item.getItemId()) {
             case R.id.add_shop:
-
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogLayout = inflater.inflate(R.layout.dialog_add_shop, null);
+                AlertDialog addNewShopDialog = new AlertDialog.Builder(this)
+                        .setTitle(R.string.adding_new_shop)
+                        .setMessage(R.string.dialog_add_shop)
+                        .setView(dialogLayout)
+                        .setPositiveButton(R.string.add_new_shop, null)
+                        .setNegativeButton(R.string.cancel, null)
+                        .create();
+                addNewShopDialog.setOnShowListener(dialog -> {
+                    Button positiveButton = addNewShopDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    final EditText shopNameEditText = (EditText) dialogLayout.findViewById(R.id.name_edit_text);
+                    final EditText shopAddressEditText = (EditText) dialogLayout.findViewById(R.id.address_edit_text);
+                    positiveButton.setOnClickListener(v -> {
+                        if (shopNameEditText.getText().length() == 0) {
+                            Toast.makeText(this, getResources().getString(R.string.enter_shop_name), Toast.LENGTH_SHORT).show();
+                        } else if (shopAddressEditText.getText().length() == 0) {
+                            Toast.makeText(this, getResources().getString(R.string.enter_shop_address), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Shop shopToAdd = new Shop(shopNameEditText.getText().toString(), shopAddressEditText.getText().toString());
+                            dao.addShop(shopToAdd);
+                            updateUI();
+                            addNewShopDialog.dismiss();
+                        }
+                    });
+                });
+                addNewShopDialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -83,6 +113,13 @@ public class ShopsActivity extends AppCompatActivity implements PopupMenu.OnMenu
         }
 
         return false;
+    }
+
+    private void updateUI() {
+        List<Shop> shops = dao.fetchAllShops();
+        ShopsListAdapter adapter = new ShopsListAdapter(shops);
+        adapter.setOnMenuItemClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 }
 
